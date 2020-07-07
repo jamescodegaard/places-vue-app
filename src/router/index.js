65c2d1b16b5aa578f1e13e8ee415d939eaa8dca2
jamescodegaard <jamescodegaard@gmail.com>
2020-07-07 10:34:42 -0500

    initial commit and adds index RESTful route

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,33 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+import About from '../views/About.vue';
+import PlacesIndex from '../views/PlacesIndex.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-  const routes = [
+const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: 'home',
     component: Home
   },
   {
     path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    name: 'about',
+    component: About
+  },
+  {
+    path: '/places',
+    name: 'places-index',
+    component: PlacesIndex
   }
-]
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+export default router;
---
+++ src/views/PlacesIndex.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="places-index">
+    <div v-for="place in places">
+      <h2>{{ place.name }}</h2>
+      <p>{{ place.address }}</p>
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function() {
+    return {
+      places: []
+    };
+  },
+  created: function() {
+    axios
+      .get("/api/places")
+      .then(response => {
+        response.data;
+        this.places = response.data;
+      })
+      .catch();
+  },
+  methods: {}
+};
+</script>
